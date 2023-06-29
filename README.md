55e4f162e82749d8048626d8230df89ba5966042
AnYouZheng <dkny710205@gmail.com>
2023-06-29 21:28:00 +0800

    create: activity.py, archi.txt folder sql_app delete: test.txt

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Calandar_platform-api
+
+# sql_app is for storage the user and the activity 
+# activiy_app is for manipulate all the changes of activity 
---
+++ sql_app/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+#SQL詳細資料
+host = 'web.csie2.nptu.edu.tw'
+user = 'cbb110111'
+password = 'aa690826'
+database = 'cbb110111_Calandar_Platform'
+
+class DB_connect:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db" 
+    #定義DB 位址
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    #創建SQLalchmy引擎並連接
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    #創建資料庫對話
+    Base = declarative_base()
+    #創建base
+    Base.metdata.create_all(bind=engine)
+    
