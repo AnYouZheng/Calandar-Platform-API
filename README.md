eb6cf940a63fa8f619d5ec7b65a2ed8d6eeefd95
AnYouZheng <dkny710205@gmail.com>
2023-07-01 14:08:36 +0800

    Move: archi.txt into README.md Delete: archi.txt Add: developement.env Feat: insert sql information into database.py from developement.env

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # Calandar_platform-api
 
+
+# architecture of sql is 
+# .
+# └── sql_app
+#    ├── __init__.py
+#    ├── crud.py
+#    ├── database.py
+#    ├── main.py
+#    ├── models.py
+#    └── schemas.py
 # sql_app is for storage the user and the activity 
 # activiy_app is for manipulate all the changes of activity 
--- sql_app/database.py
+++ sql_app/database.py
@@ -1,21 +1,31 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv, dotenv_values
 
-#SQL詳細資料
-host = ''
-user = ''
-password = ''
-database = ''
+config = dotenv_values(".env")
+load_dotenv()
 
 class DB_connect:
-    SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://user:password@host/database' 
+
+    #從.env 抓出SQL詳細資料
+    host = os.getenv('host')
+    user = os.getenv('user')
+    password = os.getenv('password')
+    database = os.getenv('database')
+    
     #定義DB 位址
-    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://user:password@host/database' 
+    
     #創建SQLalchmy引擎並連接
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    
     #創建資料庫對話
-    Base = declarative_base()
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    
     #創建base
+    Base = declarative_base()
+    
     Base.metdata.create_all(bind=engine)
     
