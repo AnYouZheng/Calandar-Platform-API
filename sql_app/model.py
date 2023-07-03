52c7f0e700b6b055bf8bf125f7bad8c862a64bc2
AnYouZheng <dkny710205@gmail.com>
2023-07-03 22:45:52 +0800

    update: fix bug

--- sql_app/model.py
+++ sql_app/model.py
@@ -5,14 +5,14 @@ from .database import Base
 #創建 table:activity 
 class activity(Base):
     __tablename__ = 'activity'
-    activity_id:    Column(Integer, primary_key=True)
-    activity_name:  Column(String,  unique=True)
-    activity_date:  Column(Date)
+    activity_id   = Column(Integer, primary_key=True)
+    activity_name = Column(String,  unique=True)
+    activity_date = Column(Date)
 
 #創建 table:TODO   
 class TODO(Base):
     __tablename__ = 'TODO'
-    TODO_id:    Column(Integer, primary_key=True)
-    TODO_name:  Column(String,  unique=True)
-    TODO_date:  Column(Date)
-    doneOrNot:  Column(Boolean)
+    TODO_id     =   Column(Integer, primary_key=True)
+    TODO_name   =   Column(String,  unique=True)
+    TODO_date   =   Column(Date)
+    doneOrNot   =   Column(Boolean)
