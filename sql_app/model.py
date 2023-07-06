469af533ff9ee58e6e0a1cb96bf8a40f9d241726
AnYouZheng <dkny710205@gmail.com>
2023-07-06 15:01:30 +0800

    update: value: descripe create: sql database in phpMyadmin -7/5

--- sql_app/model.py
+++ sql_app/model.py
@@ -1,24 +1,24 @@
-from sqlalchemy import Boolean, Column, Date, Integer, String
+from sqlalchemy import ForeignKey, Null, Boolean, Column, Date, Integer, String
 from sqlalchemy.orm import relationship
 from .database import Base
 
 #創建 table:activity 
 class activity(Base):
-    __tablename__ = 'activity'
+    __tablename__ = 'Activitys'
     activity_id   = Column(Integer, primary_key=True)
     activity_name = Column(String,  unique=True)
     activity_date = Column(Date)
+    activity_describe = Column(String, index = True)
+
+    T_id   = Column(Integer, ForeignKey("TODO.TODO_id"))
 
 #創建 table:TODO   
 class TODO(Base):
-    __tablename__ = 'TODO'
+    __tablename__ = 'TODOs'
     TODO_id     =   Column(Integer, primary_key=True)
     TODO_name   =   Column(String,  unique=True)
     TODO_date   =   Column(Date)
-    doneOrNot   =   Column(Boolean)
-    
-#創建 table:ID
-class ID(Base):
-    __tablename__ = 'IDs'
-    act_id = Column(Integer, primary_key=True)
-    todo_id = Column(Integer, primary_key=True)
+    doneOrNot   =   Column(Boolean, default= False)
+    TODO_descripe = Column(String, index = True)
+
+    A_id    = Column(Integer, ForeignKey("activity.activity_id"))
